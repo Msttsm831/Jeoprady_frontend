081fd64e5a127f05723396e199c8cc95260c2439
msttsm831 <Mohdthamer02@gmail.com>
2025-05-06 00:54:19 +0300

    sign in and sign up modules added

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,9 +1,8 @@
 import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router';
-
 import { signIn } from '../../services/authService';
-
 import { UserContext } from '../../contexts/UserContext';
+import styles from './SignInForm.module.css';
 
 const SignInForm = () => {
   const navigate = useNavigate();
@@ -22,10 +21,7 @@ const SignInForm = () => {
   const handleSubmit = async (evt) => {
     evt.preventDefault();
     try {
-      // This function doesn't exist yet, but we'll create it soon.
-      // It will cause an error right now
       const signedInUser = await signIn(formData);
-
       setUser(signedInUser);
       navigate('/');
     } catch (err) {
@@ -34,41 +30,37 @@ const SignInForm = () => {
   };
 
   return (
-    <main>
+    <main className={styles.container}>
       <h1>Sign In</h1>
-      <p>{message}</p>
-      <form autoComplete='off' onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='email'>Username:</label>
-          <input
-            type='text'
-            autoComplete='off'
-            id='username'
-            value={formData.username}
-            name='username'
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password:</label>
-          <input
-            type='password'
-            autoComplete='off'
-            id='password'
-            value={formData.password}
-            name='password'
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <button>Sign In</button>
-          <button onClick={() => navigate('/')}>Cancel</button>
+      {message && <p className={styles.message}>{message}</p>}
+      <form onSubmit={handleSubmit} className={styles.formBox}>
+        <label htmlFor='username'>Username:</label>
+        <input
+          type='text'
+          id='username'
+          value={formData.username}
+          name='username'
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor='password'>Password:</label>
+        <input
+          type='password'
+          id='password'
+          value={formData.password}
+          name='password'
+          onChange={handleChange}
+          required
+        />
+
+        <div className={styles.buttons}>
+          <button type='submit'>Sign In</button>
+          <button type='button' onClick={() => navigate('/')}>Cancel</button>
         </div>
       </form>
     </main>
   );
 };
 
-export default SignInForm
+export default SignInForm;
